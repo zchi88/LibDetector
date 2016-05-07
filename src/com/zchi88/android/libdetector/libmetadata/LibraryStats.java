ada4eed91e2d02d5ecffccf80bf5800d1a955b48
Mike <zhihaochi@gmail.com>
2016-05-06 22:38:28 -0400

    The library stats object organizes match result data

---
+++ src/com/zchi88/android/libdetector/libmetadata/LibraryStats.java
@@ -0,0 +1,50 @@
+package com.zchi88.android.libdetector.libmetadata;
+
+public class LibraryStats {
+	public LibraryStats(String libName, String versionName, int versionAge, double levRatio){
+		this.libName = libName;
+		this.versionName = versionName;
+		this.versionAge = versionAge;
+		this.levenshteinToCharRatio = levRatio;
+		this.libCount = 0;
+	}
+	
+	private String libName;
+	
+	private String versionName;
+	
+	private int libCount;
+	
+	private int versionAge;
+	
+	private double levenshteinToCharRatio;
+	
+	
+	
+	
+	public String getLibName() {
+		return libName;
+	}
+
+	public String getVersionName() {
+		return versionName;
+	}
+
+	public int getLibCount() {
+		return libCount;
+	}
+	
+	public void incrementLibCount() {
+		libCount++;
+	}
+
+
+	public int getVersionAge() {
+		return versionAge;
+	}
+
+	public double getLevenshteinToCharRatio() {
+		return levenshteinToCharRatio;
+	}
+	
+}
